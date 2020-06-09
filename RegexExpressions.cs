8e97befeddc0521874c672bcf2b2bc887dc2cb76
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-06-09 21:56:51 +0100

    finished RegexExpressions

--- RegexExpressions.cs
+++ RegexExpressions.cs
@@ -9,22 +9,39 @@ namespace Funcionarios
 {
     public static class RegexExpressions
     {
+        // Useful website to create regex expressions: https://regexr.com/
+
         private static Regex rgInt = new Regex(@"^[0-9]+");
         private static Regex rgDouble = new Regex(@"^[0-9]+(.[0-9]+)?");
+        private static Regex rgEmail = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
 
         public static bool isInteger(String s)
         {
-            return rgInt.IsMatch(s.Trim());
+            return validate(rgInt, s);
         }
 
         public static bool isDouble(String s)
         {
-            return rgDouble.IsMatch(s.Trim());
+            return validate(rgDouble, s);
         }
 
         public static bool isEmail(String s)
         {
-            // TODOOO
+            return validate(rgEmail, s);
+        }
+
+        private static bool validate(Regex r, String s)
+        {
+            // Remove white spaces
+            s = s.Trim();
+            if (s.Length == 0)
+                return false;
+            // Get match length
+            int len = r.Match(s).Length;
+            // Check if match length is same as string
+            // (If not, the string has chars that do not match the pattern)
+            if (len == s.Length)
+                return true;
             return false;
         }
     }
