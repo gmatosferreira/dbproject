58311414fdc5a427429da63cb530a92fc28b2015
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-06-10 00:13:35 +0100

    created View on NaoDocentes

--- NaoDocentes.cs
+++ NaoDocentes.cs
@@ -395,7 +395,7 @@ namespace Funcionarios
             loadTurnos();
 
             // Execute SQL query to get Docente rows
-            SqlCommand cmd = new SqlCommand("SELECT Pessoa.NMec, Pessoa.nome, Pessoa.telemovel, Funcionario.Salario, NaoDocente.turno, CONCAT(email, '@', dominio) AS emailComposed FROM(( (GestaoEscola.NaoDocente JOIN GestaoEscola.Funcionario ON NaoDocente.NMec = Funcionario.PNMec) JOIN GestaoEscola.Pessoa ON NaoDocente.NMec = Pessoa.NMec) JOIN GestaoEscola.EmailDominio ON Pessoa.emailDominio = EmailDominio.id)", cn);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM vw_NaoDocentes", cn);
             SqlDataReader reader = cmd.ExecuteReader();
             // Create list of Objects given the query results
             List<NaoDocente> tuplos = new List<NaoDocente>();
