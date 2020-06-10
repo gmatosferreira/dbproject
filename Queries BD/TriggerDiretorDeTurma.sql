08a8aa85da6e84fa3d781026173dc4aa034bccc5
Maria Inês Rocha <ines.seabrarocha@ua.pt>
2020-06-10 19:01:31 +0100

    Trigger Diretores de Turma

--- Queries BD/TriggerDiretorDeTurma.sql
+++ Queries BD/TriggerDiretorDeTurma.sql
@@ -1,19 +1,27 @@
 --select * from GestaoEscola.Turma;
 --select * from GestaoEscola.Docente;
-
 CREATE TRIGGER GestaoEscola.TriggerDiretorDeTurma on GestaoEscola.Turma
-AFTER INSERT,UPDATE AS
-BEGIN
-	IF EXISTS(SELECT diretorDeTurma FROM GestaoEscola.Turma WHERE diretorDeTurma IS NOT NULL)
+INSTEAD OF INSERT,UPDATE AS BEGIN
+IF (SELECT count(*) FROM inserted) = 1
+	BEGIN
+ 		DECLARE @nivelT int;
+		DECLARE @nomeT varchar(5);
+		DECLARE @diretorTurma int;
+		DECLARE @ano int;
+	
+		SELECT @nivelT=nivel, @nomeT=nome, @diretorTurma=diretorDeTurma, @ano=anoLetivo FROM INSERTED
+		IF EXISTS(SELECT diretorDeTurma FROM GestaoEscola.Turma WHERE diretorDeTurma=@diretorTurma)
+					RAISERROR ('Este docente já é diretor de uma turma!',16,1); 
+		ELSE
 			BEGIN
-				RAISERROR ('Este docente já é diretor de uma turma!',16,1); 
-				ROLLBACK TRAN;                                               
+				IF EXISTS(SELECT * FROM INSERTED) AND EXISTS(SELECT * FROM DELETED) -- update
+					UPDATE GestaoEscola.Turma SET diretorDeTurma = @diretorTurma WHERE (nivel = @nivelT AND nome = @nomeT AND anoLetivo = @ano);
+				IF EXISTS(SELECT * FROM INSERTED) AND NOT EXISTS(SELECT * FROM DELETED) -- insert
+					INSERT INTO GestaoEscola.Turma VALUES (@nivelT, @nomeT,@diretorTurma, @ano);
 			END
+	END
 END
-
-
-
-
+--drop trigger GestaoEscola.TriggerDiretorDeTurma
 
 
 
