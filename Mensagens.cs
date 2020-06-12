ba84dbece84fb3c844008743ae19f476bfdd12fd
Gonçalo Matos <gmatos.ferreira@ua.pt>
2020-06-13 00:12:12 +0100

    Mensagens bug fix

--- Mensagens.cs
+++ Mensagens.cs
@@ -32,6 +32,7 @@ namespace Funcionarios
         private void Mensagens_Load(object sender, EventArgs e)
         {
             flowLayoutPanel1.Controls.Clear();
+            
             String cmdTxt = "SELECT data,hora,mensagem,assunto,docente, nome FROM GestaoEscola.Anuncio JOIN GestaoEscola.EstudanteTurma ON (EstudanteTurma.turmaNivel =Anuncio.turmaNivel AND EstudanteTurma.turmaNome=Anuncio.turmaNome AND EstudanteTurma.anoLetivo= Anuncio.anoLetivo) JOIN GestaoEscola.Pessoa ON docente=NMec WHERE telemovel IS NOT NULL AND estudanteNMec=@nmec; ";
             SqlCommand cmd = new SqlCommand(cmdTxt, cn);
             cmd.Parameters.AddWithValue("@nmec", est.nmec);
@@ -55,7 +56,7 @@ namespace Funcionarios
             String cmdTxt2 = "SELECT Pessoa.nome,[data],assunto,mensagem FROM GestaoEscola.Recado JOIN GestaoEscola.Pessoa ON docenteNMEC=NMec WHERE telemovel IS NOT NULL AND estudanteNMec=@nmec; ";
             SqlCommand cmd2 = new SqlCommand(cmdTxt2, cn);
             cmd2.Parameters.AddWithValue("@nmec", est.nmec);
-            SqlDataReader reader2 = cmd.ExecuteReader();
+            SqlDataReader reader2 = cmd2.ExecuteReader();
             while (reader2.Read())
             {
                 mensagem m2 = new mensagem(this);
